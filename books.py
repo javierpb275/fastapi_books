2f65c3f44127ce8ce79d28008c8d6e1c9c06ff76
javierpb275 <javierpb275@gmail.com>
2023-05-24 20:42:32 +0200

    get endpoints with path and query parameters

--- books.py
+++ books.py
@@ -2,7 +2,73 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+BOOKS = [
+    {
+        'title': 'Title One',
+        'author': 'Author One',
+        'category': 'Science'
+    },
+    {
+        'title': 'Title Two',
+        'author': 'Author Two',
+        'category': 'Math'
+    },
+    {
+        'title': 'Title One',
+        'author': 'Author Three',
+        'category': 'History'
+    },
+    {
+        'title': 'Title Four',
+        'author': 'Author Two',
+        'category': 'Science'
+    },
+    {
+        'title': 'Title Three',
+        'author': 'Author Three',
+        'category': 'History'
+    },
+    {
+        'title': 'Title Four',
+        'author': 'Author Four',
+        'category': 'Math'
+    }
+]
 
-@app.get("/api/hello")
-def first_api():
-    return {"message": "Hello World"}
+
+@app.get("/api/books")
+async def read_all_books():
+    return BOOKS
+
+
+@app.get("/api/books/mybook")
+async def read_my_book():
+    return {'book': 'My favorite book'}
+
+
+@app.get("/api/books/{book_title}")
+async def read_book_by_title(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+
+    return {'message': 'Book Not Found'}
+
+
+@app.get("/api/books/")
+async def find_books_by_query_category(category: str):
+    books = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books.append(book)
+    return books
+
+
+@app.get("/api/books/{book_author}/")
+async def find_books_by_path_author_and_by_query_category(book_author: str, category: str):
+    books = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and book.get(
+                'category').casefold() == category.casefold():
+            books.append(book)
+    return books
