8d09d730fec58d501c6a3b6f24c97582b7f3383d
javierpb275 <javierpb275@gmail.com>
2023-05-27 11:54:47 +0200

    data validation with pydantic

--- books_2.py
+++ books_2.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -18,6 +21,24 @@ class Book:
         self.rating = rating
 
 
+class BookRequest(BaseModel):
+    id: Optional[int] = Field(title='id is not needed')
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=-1, lt=6)  # 0 - 5
+
+    class Config:
+        schema_extra = {
+            'example': {
+                'title': 'New Book',
+                'author': 'New Author',
+                'description': 'New Description',
+                'rating': 5
+            }
+        }
+
+
 BOOKS = [
     Book(1, 'Computer Science', 'Javier', 'Great book', 5),
     Book(2, 'FastAPI', 'Roby', 'Very nice book!', 5),
@@ -31,3 +52,16 @@ BOOKS = [
 @app.get("/api/books")
 async def read_all_books():
     return BOOKS
+
+
+@app.post("/api/books")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.dict())
+    BOOKS.append(find_book_id(new_book))
+    return new_book
+
+
+def find_book_id(book: Book) -> Book:
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
+
